c0b178d567f6795da643a4bbe777fd2215aa3c0f
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-07 10:47:57 +0530

    updated

---
+++ advance.js
@@ -0,0 +1,29 @@
+// function functionName(){}
+
+// const functionName2 = () => {};// we can decleare function like this
+
+// var myName ="awdiz";
+// console.log(myName);
+// myName ="xyz"; // reassign
+// console.log(myName);
+// var myName ="abc"; //redeclaration
+// console.log(myName);
+
+// In var Redeclartion and Reassign is possible
+
+// let namee ="awdiz";
+// console.log(namee);
+// namee ="abc";
+// var namee ="xyz";
+// console.log(namee);
+
+//In let only redeclartion is possible
+
+// const age = 20;
+// console.log(age);
+// // age =40; //reassign is not possibe
+// // console.log(age);
+// const age = 50;
+// console.log(age); //redeclation is not possible
+
+//In const reassign and redeclation is not possible 
